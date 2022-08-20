ed6e8c7c8d1bf669af55d480a31d6072372c92bd
AmirSinaIZ <107791153+AmirSinaIZ@users.noreply.github.com>
2022-08-20 16:41:06 +0430

    Add files via upload

---
+++ app/src/main/java/logger/Log.java
@@ -0,0 +1,20 @@
+package ir.webesho.org.logger;
+
+public class Log {
+	
+	private String mTag;
+	private CharSequence mMessage;
+	
+	public Log(String tag, CharSequence message) {
+		mTag = tag;
+		mMessage = message;
+	}
+	
+	public String getTag() {
+		return mTag;
+	}
+	
+	public CharSequence getMessage() {
+		return mMessage;
+	}
+}
